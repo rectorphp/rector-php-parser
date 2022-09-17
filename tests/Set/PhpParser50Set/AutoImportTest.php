34b32df12ab8a85548731394c059fe2b39e4f811
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-09-17 14:13:54 +0700

    final touch: add auto import test

---
+++ tests/Set/PhpParser50Set/AutoImportTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PhpParser\Tests\Set\PhpParser50Set;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class AutoImportTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/FixtureAutoImport');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/auto_import_configured_set.php';
+    }
+}
---
+++ tests/Set/PhpParser50Set/config/auto_import_configured_set.php
@@ -0,0 +1,11 @@
+<?php
+
+use Rector\PhpParser\Set\PhpParserSetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames();
+    $rectorConfig->sets([
+        PhpParserSetList::PHP_PARSER_50
+    ]);
+};
