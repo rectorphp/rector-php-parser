b2a43e05ee205c162cc50d27affdc748adbb67a8
GitHub Action <action@github.com>
2022-09-17 07:01:16 +0000

    [ci-review] Rector Rectify

--- tests/Set/PhpParser50Set/config/configured_set.php
+++ tests/Set/PhpParser50Set/config/configured_set.php
@@ -1,10 +1,10 @@
 <?php
 
-use Rector\PhpParser\Set\PhpParserSetList;
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
+use Rector\PhpParser\Set\PhpParserSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        PhpParserSetList::PHP_PARSER_50
-    ]);
+    $rectorConfig->sets([PhpParserSetList::PHP_PARSER_50]);
 };
