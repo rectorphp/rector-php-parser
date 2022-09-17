b3a54e8533e3d8a1680b4fef95b013fda571ddb1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-09-17 14:00:26 +0700

    implemented

--- config/sets/php-parser-50.php
+++ config/sets/php-parser-50.php
@@ -8,6 +8,11 @@ use Rector\Renaming\Rector\Name\RenameClassRector;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         'PhpParser\Node\Expr\ArrayItem' => 'PhpParser\Node\ArrayItem',
-        // more here
+        'PhpParser\Node\Expr\ClosureUse' => 'PhpParser\Node\ClosureUse',
+        'PhpParser\Node\Scalar\EncapsedStringPart' => 'PhpParser\Node\InterpolatedStringPart',
+        'PhpParser\Node\Scalar\LNumber' => 'PhpParser\Node\Scalar\Int_',
+        'PhpParser\Node\Stmt\DeclareDeclare' => 'PhpParser\Node\DeclareItem',
+        'PhpParser\Node\Stmt\PropertyProperty' => 'PhpParser\Node\PropertyItem',
+        'PhpParser\Node\Stmt\StaticVar' => 'PhpParser\Node\StaticVar',
     ]);
 };
