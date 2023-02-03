9639ef9429f74c14fdc3c7dad9d3b0d916663de0
Tomas Votruba <tomas.vot@gmail.com>
2023-02-03 18:17:36 +0000

    bump to PHPUnit 10

--- ecs.php
+++ ecs.php
@@ -8,7 +8,13 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([SetList::PSR_12, SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE]);
 
-    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/config', __DIR__ . '/ecs.php']);
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/config',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/easy-ci.php',
+    ]);
 
     $ecsConfig->skip(['*/Source/*', '*/Fixture/*']);
 
